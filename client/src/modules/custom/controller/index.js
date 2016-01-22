f689ee8f5acf075149010efbbce2a1f37a7ea898
gilbert <gilbert_gan@sina.com>
2016-01-22 20:04:11 +0800

    update.

--- client/src/modules/custom/controller/index.js
+++ client/src/modules/custom/controller/index.js
@@ -1,7 +1,11 @@
 'use strict';
 
-module.exports = ['$scope', 'Sprite', 'modelModel', '$state',
-  function($scope, Sprite, modelModel, $state){
-    $state.go('custom.home');
+module.exports = ['$scope', 'Sprite', 'modelModel', '$state', '$rootScope',
+  function($scope, Sprite, modelModel, $state, $rootScope){
+    //$state.go('custom.home')
+    console.log($rootScope.path);
+    $rootScope.$on('$routeChangeSuccess', function(newV) {
+      console.log($rootScope.path);
+    });
   }
 ]
