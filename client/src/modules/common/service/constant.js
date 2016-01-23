eb4af9bb6a772e654f1513aa5005745184ecc63c
gilbert <gilbert_gan@sina.com>
2016-01-23 23:11:57 +0800

    fix.

--- client/src/modules/common/service/constant.js
+++ client/src/modules/common/service/constant.js
@@ -1,10 +1,13 @@
 'use strict';
 
-var baseUrl;
-if(location.href.indexOf('localhost') != -1){
-  baseUrl = 'http://localhost:8011';
-} else {
-  baseUrl = 'http://115.28.29.138:8011';
+var Constant = {
+  baseUrl: '',
 }
 
-module.exports = baseUrl;
+if(location.href.indexOf('localhost') != -1){
+  Constant.baseUrl = 'http://localhost:8011';
+} else {
+  Constant.baseUrl = 'http://115.28.29.138:8011';
+}
+
+module.exports = Constant;
