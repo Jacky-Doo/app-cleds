e7563934552b7d3e3b0f8ed454f64384c9e36967
gilbert <gilbert_gan@sina.com>
2016-01-23 21:41:48 +0800

    fix baseUrl for alien.

--- client/src/modules/common/service/constant.js
+++ client/src/modules/common/service/constant.js
@@ -3,3 +3,9 @@
 module.exports = {
   baseUrl: 'http://localhost:8011',
 }
+var baseUrl;
+if(location.href.indexOf('localhost') != -1){
+  baseUrl = 'http://localhost:8011';
+} else {
+  baseUrl = 'http://115.28.29.138:8011';
+}
