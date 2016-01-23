7a3a6589cf9bb291bc3673f90ecdd94a0cd4ad71
gilbert <gilbert_gan@sina.com>
2016-01-23 22:51:50 +0800

    fix.

--- client/src/modules/common/service/constant.js
+++ client/src/modules/common/service/constant.js
@@ -1,11 +1,10 @@
 'use strict';
 
-module.exports = {
-  baseUrl: 'http://localhost:8011',
-}
 var baseUrl;
 if(location.href.indexOf('localhost') != -1){
   baseUrl = 'http://localhost:8011';
 } else {
   baseUrl = 'http://115.28.29.138:8011';
 }
+
+module.exports = baseUrl;
